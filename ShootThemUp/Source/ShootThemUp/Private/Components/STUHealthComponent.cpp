b9ba9dbfab7c1b37a02798b49bb95947e737997e
Constantine Afanasiev <yamatoneedzoky@gmail.com>
2021-12-13 19:41:41 +0200

    Excersise 39. Health Regen

--- ShootThemUp/Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ ShootThemUp/Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -3,7 +3,8 @@
 
 #include "Components/STUHealthComponent.h"
 #include "GameFramework/Actor.h"
-
+#include "Engine/World.h"
+#include "TimerManager.h"
 
 
 DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All)
@@ -23,8 +24,7 @@ void USTUHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Health = MaxHealth;
-	OnHealthChanged.Broadcast(Health);
+	SetHealth(MaxHealth);
 
 	AActor* ComponentOwner = GetOwner();
 	if(ComponentOwner)
@@ -37,12 +37,33 @@ void USTUHealthComponent::BeginPlay()
 void USTUHealthComponent::OnTakeAnyDamage(AActor* DamageActor, float Damage, const UDamageType* DamageType
 	, AController* InstigatedBy, AActor* DamageCauser)
 {
-	if(Damage <=0.0f || IsDead()) return;
+	if(Damage <=0.0f || IsDead() || !GetWorld()) return;
 	
-	Health = FMath::Clamp(Health - Damage, 0.0f, MaxHealth);
-	OnHealthChanged.Broadcast(Health);
+	SetHealth(Health - Damage);
+	GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
+	
 	if(IsDead())
 	{
 		OnDeath.Broadcast();
 	}
+	else if (AutoHeal)
+	{
+		GetWorld()->GetTimerManager().SetTimer(HealTimerHandle, this, &USTUHealthComponent::HealUpdate, HealUpdateTime, true, HealDelay);
+	}
+}
+
+void USTUHealthComponent::HealUpdate()
+{
+	SetHealth(Health + HealAmount);
+
+	if(FMath::IsNearlyEqual(Health, MaxHealth) && GetWorld())
+	{
+		GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
+	}
+}
+
+void USTUHealthComponent::SetHealth(float NewHealth)
+{
+	Health = FMath::Clamp(NewHealth, 0.0f, MaxHealth);
+	OnHealthChanged.Broadcast(Health);
 }
--- ShootThemUp/Source/ShootThemUp/Public/Components/STUHealthComponent.h
+++ ShootThemUp/Source/ShootThemUp/Public/Components/STUHealthComponent.h
@@ -21,7 +21,7 @@ public:
 	float GetHealth() const {return Health;};
 
 	UFUNCTION(BlueprintCallable)
-	bool IsDead() const {return Health <=0.0f;}
+	bool IsDead() const {return FMath::IsNearlyZero(Health);}
 
 	FOnDeath OnDeath;
 	FOnHealthChanged OnHealthChanged;
@@ -30,12 +30,28 @@ protected:
 	// Called when the game starts
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (ClampMin = "0", ClampMax="100"))
 	float MaxHealth = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Health Regen")
+	bool AutoHeal = true;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Health Regen", meta = (EditCondition = "AutoHeal"))
+	float HealUpdateTime = 1.0f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Health Regen", meta = (EditCondition = "AutoHeal"))
+	float HealDelay = 3.0f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Health Regen", meta = (EditCondition = "AutoHeal"))
+	float HealAmount = 5.0f;
 	
 	virtual void BeginPlay() override;
 
 private:
 	float Health = 0.0f;
+	FTimerHandle HealTimerHandle;
 	
 	UFUNCTION()
 	void OnTakeAnyDamage(AActor* DamageActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+	
+	void HealUpdate();
+	void SetHealth(float NewHealth);
 };
