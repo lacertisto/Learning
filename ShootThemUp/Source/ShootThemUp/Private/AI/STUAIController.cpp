1683234b7126d8b4cd533d73bc31b575afc82f3f
Constantine Afanasiev <yamatoneedzoky@gmail.com>
2022-01-10 17:41:33 +0200

    Add perception to the AI

--- ShootThemUp/Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ ShootThemUp/Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -3,6 +3,14 @@
 
 #include "AI/STUAIController.h"
 #include "AI/STUAICharacter.h"
+#include "Components/STUAIPerceptionComponent.h"
+
+ASTUAIController::ASTUAIController()
+{
+	STUAIPerceptionComponent = CreateDefaultSubobject<USTUAIPerceptionComponent>("STUPerceptionComponent");
+	SetPerceptionComponent(*STUAIPerceptionComponent);
+}
+
 
 void ASTUAIController::OnPossess(APawn* InPawn)
 {
@@ -14,3 +22,11 @@ void ASTUAIController::OnPossess(APawn* InPawn)
 		RunBehaviorTree(STUCharacter->BehaviorTreeAsset);
 	}
 }
+
+void ASTUAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	const auto AimActor = STUAIPerceptionComponent->GetClosestEnemy();
+	SetFocus(AimActor);
+}
+
