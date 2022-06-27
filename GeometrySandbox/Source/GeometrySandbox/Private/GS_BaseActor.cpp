4e53c7bfad1567449c5512ebbabf10881fab84dc
Constantine Afanasiev <yamatoneedzoky@gmail.com>
2022-06-27 23:11:39 +0200

    Learned basic Struct Enum Components and log categories creation

--- GeometrySandbox/Source/GeometrySandbox/Private/GS_BaseActor.cpp
+++ GeometrySandbox/Source/GeometrySandbox/Private/GS_BaseActor.cpp
@@ -11,6 +11,8 @@ AGS_BaseActor::AGS_BaseActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("Static Mesh");
+	StaticMesh->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
@@ -18,7 +20,18 @@ void AGS_BaseActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogBaseActor, Warning, TEXT("Hello Ureal! Again :)"));
+	FString	Name = "John";
+	
+	UE_LOG(LogBaseActor, Warning, TEXT("Hello Ureal! %s Again :)"), *Name);
+
+	GEngine->AddOnScreenDebugMessage(-1,3.f,FColor::Red,Name);
+
+	FTransform ActorTransform = GetActorTransform(); // Get actors Transform
+	FVector Location = ActorTransform.GetLocation(); // Get actors location
+	FRotator Rotation = ActorTransform.Rotator(); // Get actors Rotation
+	FVector Scale = ActorTransform.GetScale3D(); // Get actors Scale
+
+	InitialLocation = GetActorLocation();
 }
 
 // Called every frame
@@ -26,5 +39,23 @@ void AGS_BaseActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	HandleMovement(DeltaTime);
+	// z - z0 + amplitude * sin(freq * t) sinusoidal movement law
+
 }
 
+void AGS_BaseActor::HandleMovement(const float &DeltaTime)
+{
+	switch (GeometryData.MovementType)
+	{
+		case EMovementType::Sin:
+		{
+			FVector CurrentLocation = GetActorLocation();
+			CurrentLocation.Z = InitialLocation.Z + GeometryData.Amplitude * FMath::Sin(GeometryData.Frequency * GetWorld()->GetTimeSeconds()) * DeltaTime;
+			SetActorLocation(CurrentLocation);
+			break;
+		}
+		case EMovementType::Static: break;
+		default: break;
+	}
+}
--- GeometrySandbox/Source/GeometrySandbox/Public/GS_BaseActor.h
+++ GeometrySandbox/Source/GeometrySandbox/Public/GS_BaseActor.h
@@ -6,6 +6,29 @@
 #include "GameFramework/Actor.h"
 #include "GS_BaseActor.generated.h"
 
+UENUM(BlueprintType)
+enum class EMovementType : uint8
+{
+	Sin,
+	Static
+};
+
+USTRUCT(BlueprintType)
+struct FGeometryData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+    float Amplitude = 50.f;
+    
+    UPROPERTY(EditAnywhere)
+    float Frequency = 2.f;
+    
+    UPROPERTY(EditAnywhere)
+    EMovementType MovementType = EMovementType::Static;
+};
+
+
 UCLASS()
 class GEOMETRYSANDBOX_API AGS_BaseActor : public AActor
 {
@@ -15,12 +38,23 @@ public:
 	// Sets default values for this actor's properties
 	AGS_BaseActor();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(EditAnywhere, Category="GeometryData")
+	FGeometryData GeometryData;	
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY()
+	float WeaponsNum = 4.f;
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	void HandleMovement(const float &DeltaTime);
+private:
+	FVector InitialLocation;
 };
