8166ed0422836d0e68d68d60a66c50a8725e5c5b
Constantine Afanasiev <yamatoneedzoky@gmail.com>
2022-11-03 20:45:53 +0100

    created project

---
+++ Caracal/Source/Caracal/CaracalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CaracalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARACAL_API ACaracalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
